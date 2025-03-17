376ef64112d8a9ff2b239a4aedaadc69b6eed480
Adam McKellar <dev@mckellar.eu>
2025-03-17 11:35:09 +0100

    chore(transport): updated cal types

--- packages/transport/src/core/CryptoLayerConfig.ts
+++
@@ -1,6 +0,0 @@
-import { AdditionalConfig, ProviderFactoryFunctions } from "crypto-layer-ts-types";
-
-export type CryptoLayerConfig = {
-    factoryFunctions: ProviderFactoryFunctions,
-    keyMetadataStoreConfig: Extract<AdditionalConfig, {"KVStoreConfig": any} | {"FileStoreConfig": any}>
-}
